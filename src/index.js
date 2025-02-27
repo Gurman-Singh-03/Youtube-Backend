f68b46d124eb194ade9a5d1456689f7d8610c4e6
Gurman-Singh-03 <singhgurman573@gmail.com>
2025-02-27 18:48:56 +0530

    add user and vedio model

--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./db/index.js";
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
 
 dotenv.config({
     path: './env'
 });
-connectDB(); 
 
-// const app = express();
+const app = express();
 
-// (async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//         app.on("error", (error) => {
-//             console.log("ERROR: ", error);
-//             throw error;
-//         });
-
-//         app.listen(process.env.PORT, () => {
-//             console.log(`Server running on port ${process.env.PORT}`);
-//         });
-//     } catch (error) {
-//         console.error("Failed to connect to the database", error);
-//     }
-// })();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error("Failed to connect to the database", error);
+    });
